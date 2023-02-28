64cbcd0ee4bf45491f01ebfd62cf9e11fb0d7b1e
sbeyzayildiz <sumeyyebeyzayildiz@gmail.com>
2023-02-28 23:12:37 +0300

    memory game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+import { useState, useMemo, useEffect } from 'react';
 import './App.css';
+import Card from './components/Card'
+
 
 function App() {
+
+  const [frameworks, setFrameWorks] = useState(['angular2', 'vue', 'react', 'grunt', 'phantomjs', 'ember', 'babel', 'ionic', 'gulp', 'meteor', 'yeoman', 'yarn', 'nodejs', 'bower', 'browserify']);
+
+  const [duplicatedFrameWorks, setDuplicateFrameworks] = useState([]);
+
+  const [openedFrameworks, setOpenedFrameworks] = useState([]);
+
+  const [randomizedFrameworks, setRandomizedFrameworks] = useState([]);
+
+  const [finalizedFrameworks, setFinalizedFrameworks] = useState([]);
+
+  const shuffle = (array) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+
+  useEffect(() => {
+    const array = [...frameworks]
+    setDuplicateFrameworks(array.concat(array));
+  }, [frameworks])
+
+  useMemo(() => {
+    setRandomizedFrameworks(shuffle(duplicatedFrameWorks));
+  }, [duplicatedFrameWorks])
+
+  useMemo(() => {
+
+    const finallyArray = randomizedFrameworks.map((name, index) => { return { name, close: true, complete: false, fail: false } })
+
+    setFinalizedFrameworks(finallyArray);
+  }, [randomizedFrameworks])
+
+  useMemo(() => {
+      setTimeout(() => {
+        check()
+      }, 750)
+  }, [openedFrameworks])
+
+
+  const handleClick = (frame, name, index) => {
+
+      let framework = {
+        name,
+        index
+      }
+
+
+      let frameworks = [...openedFrameworks]
+      let array = [...finalizedFrameworks]
+      array[index].close = false
+
+      frameworks.push(framework)
+
+      setOpenedFrameworks(frameworks);
+
+      setFinalizedFrameworks(array)
+
+  }
+
+  const check = () => {
+    if ((openedFrameworks[0].name === openedFrameworks[1].name) && (openedFrameworks[0].index !== openedFrameworks[1].index)) {
+      finalizedFrameworks[openedFrameworks[0].index].complete = true
+      finalizedFrameworks[openedFrameworks[1].index].complete = true
+    } else {
+      finalizedFrameworks[openedFrameworks[0].index].close = true
+      finalizedFrameworks[openedFrameworks[1].index].close = true
+    }
+
+
+    setFinalizedFrameworks(finalizedFrameworks)
+    setOpenedFrameworks([]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="playground">
+      {
+        finalizedFrameworks.map((framework, index) => <Card key={index} framework={framework.name}
+          click={(frame) => { handleClick(frame, framework.name, index) }}
+          close={framework.close}
+          complete={framework.complete}
+        />)
+      }
+
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Card({ framework, close, complete, click }) {
+
+    console.log('close', close);
+
+    return (
+        <div className={"card" + (!close ? ' opened' : '') + (complete ? ' matched' : '')} onClick={() => click(framework)}>
+            <div className="front">
+                ?
+            </div>
+            <div className="back">
+                <img src={`https://raw.githubusercontent.com/samiheikki/javascript-guessing-game/master/static/logos/${framework}.png`} alt={framework} />
+            </div>
+        </div>
+    )
+}
+
+export default Card
